21f6d845d11e8a47ccbe04201e8850f50d4d0c63
rxliuli <rxliuli@gmail.com>
2021-01-07 21:10:50 +0800

    完成基本的主进程打包配置

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,10 +1,12 @@
 import { app, BrowserWindow } from 'electron'
+import path = require('path')
+import { URL } from 'url'
 
 async function createMainWindow() {
     // 创建新的 electron 窗口
     const mainWindow = new BrowserWindow()
-    // 载入开发环境的 url
-    await mainWindow.loadURL('http://localhost:3000/')
+    // 载入生产环境的 url
+    await mainWindow.loadURL(new URL(path.join(__dirname, './build/index.html')).href)
 }
 
 /**
