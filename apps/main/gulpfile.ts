e6b96c5a6719e6850b4bd5f80e503671c3fe7e21
rxliuli <rxliuli@gmail.com>
2021-01-12 12:28:17 +0800

    实现一键打包二进制程序

--- apps/main/gulpfile.ts
+++ apps/main/gulpfile.ts
@@ -1,6 +1,5 @@
-import { copy, readFile, remove } from 'fs-extra'
+import { copy, remove } from 'fs-extra'
 import * as path from 'path'
-import { parse } from 'yaml'
 
 async function copyByMap(copyMap: [string, string][]) {
     await Promise.all(
@@ -27,36 +26,5 @@ export async function copyStatic() {
     await copyByMap([
         ['../renderer/build', 'dist/build'],
     ])
-}
-
-interface LastVersionInfo {
-    version: string
-    files: [
-        {
-            url: string
-            sha512: string
-            size: number
-        },
-    ]
-    path: string
-    sha512: string
-    releaseDate: string
-}
-
-/**
- * 复制需要的二进制资源
- */
-export async function copyBuilderInfo() {
-    const baseUrl = __dirname
-    const yml = await readFile(path.resolve(baseUrl, './release/latest.yml'), {
-        encoding: 'utf8',
-    })
-    const lastInfo = parse(yml) as LastVersionInfo
-    const copyFileNameList = ['builder-effective-config.yaml', 'latest.yml', lastInfo.path, `${lastInfo.path}.blockmap`]
-    await Promise.all(
-        copyFileNameList.map((fileName) =>
-            copy(path.resolve(baseUrl, `./release/${fileName}`), path.resolve(baseUrl, `./dist/build/${fileName}`)),
-        ),
-    )
 }
 
